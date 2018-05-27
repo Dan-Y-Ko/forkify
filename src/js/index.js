69cffcc23f373c751378caf3784dd19bb284597c
Daniel Ko <dk3790@hotmail.com>
2018-05-26 23:28:16 -0400

    search-model, search-view, search-controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,55 @@
 // Global app controller
-import num from './test';
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
 
-console.log(`I imported ${num} from another module!!`);
+/* Global state of app
+** search object
+** current recipe object
+** shopping list object
+** liked recipes
+*/
+
+const state = {};
+
+const controlSearch = async () =>
+{
+    // get query from view
+    const query = searchView.getInput();
+
+    if (query)
+    {
+        // new search object and add to state
+        state.search = new Search(query);
+
+        // prepare ui for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        // search for recipes
+        await state.search.getResults();
+
+        // render results on ui
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e =>
+{
+    e.preventDefault();
+    controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e =>
+{
+    const btn = e.target.closest('.btn-inline');
+    
+    if (btn)
+    {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
